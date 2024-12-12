f1fe8050a6b42b502e81c566eecf46f082aa6807
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-12 15:06:38 +0500

    [orders-rmq-broker] 12.12.2024 HTTPException

--- src/app/routers/orders.py
+++ src/app/routers/orders.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 
 from src.app.schemas.order import OrderSchema, OrderResponse
 from src.rmq import producer
@@ -13,7 +14,12 @@ orders_router = APIRouter(
 
 
 @orders_router.post(path='/push/', response_model=OrderResponse)
-async def push_order(order: OrderSchema) -> JSONResponse:
+async def push_order(order: OrderSchema) -> JSONResponse | HTTPException:
+    if order.project != "Дисконт суши":
+        return HTTPException(
+            status_code=400,
+            detail="Project != `Дисконт суши`"
+        )
     message = utils.to_json(order.model_dump())
     await producer.publish(message=message)
     return JSONResponse(
